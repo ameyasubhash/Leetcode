0eb6b455ce132e71b06983acfd1c9e81f960e537
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-03-25 12:05:45 +0530

    Time: 1 ms (77.33%), Space: 44.2 MB (35.50%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,35 +1,67 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //brute force without space
-        for(int i =0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    for(int k=0;k<matrix.length;k++){
-                        if(matrix[k][j]!=0) 
-                         matrix[k][j]=-1234;
-                    }
-                    for(int l=0;l<matrix[0].length;l++){
-                        if(matrix[i][l]!=0) 
-                        //     matrix[i][l]=0;
-                        // else
-                            matrix[i][l]=-1234;    
-                    }
-                }
-            }
+         int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
         }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+        //optimised code change in matrix itself
+//         int col0=1,rows=matrix.length,cols=matrix[0].length;
         
-		for(int i =0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==-1234)
-                matrix[i][j]=0;
-            }
-		}
+//         for(int i=0;i<rows;i++){
+//             if(matrix[i][0]==0) col0=0; 
+//             for(int j=1;j<cols;j++){
+//                 if(matrix[i][j]==0)
+//                     matrix[i][0]=matrix[0][j]=0;
+//             }
+//         }
+//         for(int i=rows-1;i>=0;i--){
+//             for(int j=cols-1;j>=1;j--){
+//                 if(matrix[i][0]==0||matrix[0][j]==0)
+//                     matrix[i][j]=0;
+//                 if(col0==0) matrix[i][0]=0;
+//             }
+//         }
+        
+        
+        
+//         //brute force without space
+//         for(int i =0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 if(matrix[i][j]==0){
+//                     for(int k=0;k<matrix.length;k++){
+//                         if(matrix[k][j]!=0) 
+//                          matrix[k][j]=-1234;
+//                     }
+//                     for(int l=0;l<matrix[0].length;l++){
+//                         if(matrix[i][l]!=0) 
+//                         //     matrix[i][l]=0;
+//                         // else
+//                             matrix[i][l]=-1234;    
+//                     }
+//                 }
+//             }
+//         }
+        
+// 		for(int i =0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 if(matrix[i][j]==-1234)
+//                 matrix[i][j]=0;
+//             }
+// 		}
     
 
 
         //brute force using space
-
-
     //   int temp[][]=new int[matrix.length][matrix[0].length];
 	// 	 for(int i =0;i<matrix.length;i++){
     //         for(int j=0;j<matrix[0].length;j++){
