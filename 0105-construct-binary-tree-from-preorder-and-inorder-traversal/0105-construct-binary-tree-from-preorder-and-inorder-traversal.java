9372bdc3cf5341d923fa3496704b20cd447bf6b8
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-04-07 12:07:17 +0530

    Time: 2 ms (98.54%), Space: 42.3 MB (46.97%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode build(int [] preorder,int preStart, int preEnd, int [] inorder, int inStart, int inEnd, Map<Integer,Integer> inMap){
+        
+        if(preStart>preEnd || inStart>inEnd) return null;
+        
+        TreeNode root = new TreeNode(preorder[preStart]);
+        
+        int inRoot=inMap.get(root.val);
+        int numsLeft=inRoot-inStart;
+        
+        root.left=build(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,inMap);
+        
+        root.right=build(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,inMap);
+        
+        return root;
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map <Integer,Integer> inMap=new HashMap<Integer,Integer>();
+        
+        for(int i=0;i<inorder.length;i++)
+            inMap.put(inorder[i],i);
+        
+        TreeNode root= build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,inMap);
+        
+        return root;
+        
+        
+        
+        
+        
+        
+        
+//         Queue <TreeNode> q=new Linkedlist<>();
+//         Linked<TreeNode> 
+            
+//         while(!q.isEmpty()){
+//             q.offer(preorder[0]);
+//             for(int i=0;i<inorder.length;i++){
+//             q.offer(inorder[i]);
+//             if(q.peek().val==inorder[i]){
+//                 q.offer(inorder[i]);
+//             }
+//         }
+//          q.poll();   
+//         }
+       
+        
+    }
+}
