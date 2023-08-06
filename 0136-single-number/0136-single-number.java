8347660f09d052d674937b8f6572b4fcb3449ddd
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-08-06 12:23:51 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (80.90%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -13,15 +13,23 @@ class Solution {
         // }
         // return nums[nums.length-1];
         
-        //better
-        HashMap <Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // better
+        // HashMap <Integer, Integer> map = new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // for(int i=0; i<nums.length; i++){
+        //     if(map.get(nums[i])==1)
+        //         return nums[i];
+        // }
+        // return 0;
+        
+        // optimised
+        int k=0;
+       
+        for(int i:nums){
+            k^=i;
         }
-        for(int i=0; i<nums.length; i++){
-            if(map.get(nums[i])==1)
-                return nums[i];
-        }
-        return 0;
+        return k;
     }
 }
