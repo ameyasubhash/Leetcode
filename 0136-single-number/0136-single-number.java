7514a9057cd8f1c63010986a0939c14ad3ff43ba
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-08-06 12:21:37 +0530

    Time: 12 ms (26.26%), Space: 44.6 MB (62.43%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,32 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
-                Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
-            if(nums[0]!=nums[1])
-                return nums[0];
-            if(nums[i]!=nums[i+1])
-                if(nums[i+1]!=nums[i+2])
-                    return nums[i+1];
-            
+        
+        // brute force
+        // Arrays.sort(nums);
+        // for(int i=0; i<nums.length-2; i++){
+        //     if(nums[0]!=nums[1])
+        //         return nums[0];
+        //     if(nums[i]!=nums[i+1])
+        //         if(nums[i+1]!=nums[i+2]){
+        //              return nums[i+1];
+        //         }
+        // }
+        // return nums[nums.length-1];
+        
+        //better
+        HashMap <Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        return nums[nums.length-1];
-//         int k=0;
-//         int i;
-//         // brute force
-//         Arrays.sort(nums);
-//         for(i=0; i<nums.length-2; i+=2){
-//             if(nums[0]!=nums[1])
-//                 return nums[0];
-//             // if(nums[i]!=nums[i+1])
-//             //     if(nums[i+1]!=nums[i+2]){
-//             //         k=nums[i+1];
-//             //         return k;
-//             //     }
-//             if(nums[i]!=nums[i+1])
-//                 if(nums[i+1]!=nums[i+2])
-//                     return nums[i+1];
-            
-//         }
-//         return nums[nums.length-1];
+        for(int i=0; i<nums.length; i++){
+            if(map.get(nums[i])==1)
+                return nums[i];
+        }
+        return 0;
     }
 }
