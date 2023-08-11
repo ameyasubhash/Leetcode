d024602d569ae9bc78384813958f096c2f817571
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-08-11 14:04:45 +0530

    Time: 6 ms (38.10%), Space: 48.3 MB (65.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,37 @@
 class Solution {
     public int majorityElement(int[] a) {
-        int n=a.length;
-        int curmax, max=0, ele=0;
-     for(int i=0; i<n; i++){
-         curmax=0;
-         for(int j=0; j<n; j++){
-             if(a[i]==a[j])
-                 curmax++;
-         }
-      //   max= Math.max(curmax, max);
-         if(curmax>max){
-            ele=a[i]; 
-            max=curmax; 
-         }
-     }
-        return ele;
+        // brute force
+     //    int n=a.length;
+     //    int curmax, max=0, ele=0;
+     // for(int i=0; i<n; i++){
+     //     curmax=0;
+     //     for(int j=0; j<n; j++){
+     //         if(a[i]==a[j])
+     //             curmax++;
+     //     }
+     //  //   max= Math.max(curmax, max);
+     //     if(curmax>max){
+     //        ele=a[i]; 
+     //        max=curmax; 
+     //     }
+     // }
+     //    return ele;
+        
+        // intermediate
+        Arrays.sort(a);
+        int curmax=0, max=0, num=a[0];
+        for(int i=0; i<a.length-1; i++){
+            if(a[i]==a[i+1])
+                curmax++;
+            else{
+                curmax=0;
+            }
+            if(max<curmax){
+                    num=a[i];
+                    max=curmax;
+                    
+                }
+        }
+        return num;
     }
 }
