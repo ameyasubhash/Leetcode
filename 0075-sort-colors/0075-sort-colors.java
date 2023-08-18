ee2c4f3f7b5f4982d9c49140009cac15b946fd91
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-08-18 19:31:08 +0530

    Time: 1 ms (21.83%), Space: 40.4 MB (99.30%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -7,18 +7,29 @@ class Solution {
     
     
     public void sortColors(int[] a) {
+        int n=a.length;
+        
         
         // bubble sort
+        // for(int i=0; i<n-1; i++){
+        //     boolean swap = false;
+        //     for(int j=0; j<n-1-i; j++){
+        //         if(a[j+1]<a[j]){
+        //             swap= true;
+        //             swap(a, j+1, j);
+        //         }
+        //     }
+        // }
         
-        int n=a.length;
-        for(int i=0; i<n-1; i++){
-            boolean swap = false;
-            for(int j=0; j<n-1-i; j++){
-                if(a[j+1]<a[j]){
-                    swap= true;
-                    swap(a, j+1, j);
-                }
+        // insertion sort
+        for(int i=1; i<n; i++){
+            int j=i-1;
+            int temp= a[i];
+            while(j>=0 && a[j]>temp){
+                a[j+1]=a[j];
+                j--;
             }
+            a[j+1]=temp;
         }
     }
 }
