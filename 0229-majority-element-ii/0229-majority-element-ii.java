46db2301cece34099b54acf78e25ad92b106e564
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-10-08 14:21:50 +0530

    Time: 2 ms (97.33%), Space: 46.5 MB (60.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,56 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-//         int n=nums.length;
-//         int c1=0, c2=0, k1=nums[0], k2=nums[0];
-//         List<Integer> list=new ArrayList<>();
+        List<Integer> list=new ArrayList<Integer>();
         
+        int c1=0, c2=0, n1=0, n2=0, n=nums.length;
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]==nums[n1]) c1++;
+            else if(nums[i]==nums[n2]) c2++;
+            else if(c1==0){ n1=i; c1++;}
+            else if(c2==0){ n2=i; c2++;}
+            else{c1--; c2--;}
+        }
+        c1=0; c2=0; n1=nums[n1]; n2=nums[n2];
+        
+        for(int num:nums){
+            if(n1==num) c1++;
+            else if(n2==num) c2++;
+        }
+        if(c1>(n/3)) list.add(n1);
+        if(c2>n/3) list.add(n2);
+        
+        return list;
+        
+//         List<Integer> list= new ArrayList<Integer>();
+        
+//         int key=0, count=1, n=nums.length ;
 //         for(int i=0; i<n; i++){
-//             if(nums[i]==k1)
-//                 c1++;
-//             else
-//                 count--;
-//             if(count==0){
-//                 key=nums[i];
-//                 count=1;
-//             }
+//             if(nums[])
 //         }
-//         return list;
-        
-         List<Integer> ans = new ArrayList<>();
-        
-        int n = nums.length;
-        
-        int numOne = 0, numTwo = 0, countOne = 0, countTwo = 0;
-        
-        for(int num : nums){
-            if(num == numOne) countOne++;
-            else if(num == numTwo) countTwo++;
-            else if(countOne == 0){
-                numOne = num;
-                countOne++;
-            }else if(countTwo == 0){
-                numTwo = num;
-                countTwo++;
-            }else{
-                countOne--;
-                countTwo--;
-            }
-        }
-        
-        
-        countOne = 0;
-        countTwo = 0;
-        
-		
-		//To check if numOne or numTwo appears more than n/3 times
-        for(int num : nums){
-            if(numOne == num) countOne++;
-            else if(numTwo == num)countTwo++;
-        }
-        
-        if(countOne > n/3) ans.add(numOne);
-        if(countTwo > n/3) ans.add(numTwo);
-        
-        return ans;
     }
 }
